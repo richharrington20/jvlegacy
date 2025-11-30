5317fc1ba1b25a7b1e99d1917cd0cea257043a84
Rich Harrington <rich@maintainhq.com>
2025-11-30 00:54:24 +0000

    Add fillable fields to Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -9,4 +9,14 @@ class Company extends Model
     protected $connection = 'legacy';
     protected $table = 'companies';
     public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'number',
+        'email',
+        'telephone_number',
+        'website',
+        'created_on',
+        'updated_on',
+    ];
 }
