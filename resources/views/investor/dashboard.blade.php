4a1ddde3d9e32343ef547051401a247e4caaf4b5
Rich Harrington <rich@maintainhq.com>
2025-12-15 00:17:14 +0000

    Fix Documents Tab section: correct structure with @endif and closing divs

--- resources/views/investor/dashboard.blade.php
+++ resources/views/investor/dashboard.blade.php
@@ -685,6 +685,7 @@
                                     </div>
                                 @endif
                             </div>
+                        </div>
                         @endforeach
                     @else
                         <div class="bg-white border border-gray-200 rounded-lg p-6">
